f0d0d3d90eb75ea05adab65c4400bd97e491c219
babu-x <bhaskar170863@gmail.com>
2024-03-06 18:51:50 +0530

    added notfound compo

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,7 @@ import HomePage from "./containers/HomePage/HomePage"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUpPage from './containers/SignUpPage/SignUpPage';
 import LoginPage from './containers/LoginPage/LoginPage';
+import NotFound from './components/NotFound/NotFound';
 
 const App = () => {
   return(
@@ -11,7 +12,7 @@ const App = () => {
         <Route path='/' element={<HomePage />} />
         <Route path='/signup' element={<SignUpPage />} />
         <Route path='/login' element={<LoginPage />} />
-        <Route path='*' element={<h1>Not Found</h1>} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
       
     </BrowserRouter>
---
+++ app/src/components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import {Link} from 'react-router-dom';
+import './NotFound.css'
+const NotFound = () => {
+    return(
+        <div className="display-center h-100vh">
+            <img className='notfound-image' src='/notfound.png' />
+            <p className="small-margin">Woops. Looks like this page doesnot exist.</p>
+            <p className='link small-margin'>Click <Link to={'/'} className='link link-color'>here</Link> to go back home</p>
+        </div>
+    )
+}
+
+export default NotFound
